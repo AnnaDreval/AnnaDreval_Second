580196eff32682b0021632c8bb48113ba3ce1e4d
AnnaDreval <annadreval@yandex.ru>
2018-10-17 17:11:05 +0300

    lesson2 commit

---
+++ src/test/java/lesson2/SimpleTest.java
@@ -0,0 +1,66 @@
+package lesson2;
+
+import base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class SimpleTest extends TestBase {
+
+    private  WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethod() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() {
+        driver.close();
+    }
+
+    @Test(groups = "Group1")
+    public void simpleTest() {
+
+        //2 Navigate
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //3 Assert Title
+        assertEquals(driver.getTitle(), "Home Page");
+
+        //4 Login
+        driver.findElement(By.cssSelector(".profile-photo")).click();
+        driver.findElement(By.cssSelector("[id = 'Name']")).sendKeys("epam");
+        driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
+
+        //5 Check Title
+        WebElement mainTitle = driver.findElement(By.cssSelector("h3.main-title"));
+        assertEquals(mainTitle.getText(), "EPAM FRAMEWORK WISHES…");
+
+        //6 SoftAssert
+        WebElement element = driver.findElement(By.id("Name"));
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(element.isDisplayed());
+
+        //Action LogOut
+        element = driver.findElement(By.cssSelector(".login [type = 'submit']"));
+        Actions action  = new Actions(driver);
+        action.moveToElement(element).click();
+
+    }
+}
---
+++ src/test/java/lesson2/SimpleTestDataProvider.java
@@ -0,0 +1,60 @@
+package lesson2;
+
+import base.TestBase;
+import dataProviders.DataProviders;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class SimpleTestDataProvider extends TestBase {
+
+    @Test(dataProvider = "simpleDataProvider", dataProviderClass = DataProviders.class)
+    public void simpleTest(String s, int i) {
+
+        //1 Open BR
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //2 Navigate
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //3 Assert Title
+        assertEquals(driver.getTitle(), "Home Page");
+
+        //4 Login
+        driver.findElement(By.cssSelector(".profile-photo")).click();
+        driver.findElement(By.cssSelector("[id = 'Name']")).sendKeys("epam");
+        driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
+
+        //5 Check Title
+        WebElement mainTitle = driver.findElement(By.cssSelector("h3.main-title"));
+        assertEquals(mainTitle.getText(), "EPAM FRAMEWORK WISHES…");
+
+        //6 SoftAssert
+        WebElement element = driver.findElement(By.id("Name"));
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(element.isDisplayed());
+
+        //7 Action LogOut
+        element = driver.findElement(By.cssSelector(".login [type = 'submit']"));
+        Actions action  = new Actions(driver);
+        action.moveToElement(element).click();
+
+        //8 Close
+        driver.close();
+        System.out.println("String: " + s);
+        System.out.println("int: " + i);
+
+    }
+}
